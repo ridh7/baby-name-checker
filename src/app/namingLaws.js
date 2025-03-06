e0135df63e9d1392797fe389c6b26d0959aa26b7
Ridhwik Kalgaonkar <rk44@illinois.edu>
2025-03-06 10:20:40 -0600

    add naming law for Alabama

---
+++ src/app/namingLaws.js
@@ -0,0 +1,6 @@
+export const namingLaws = {
+  Alabama: {
+    rule: "The name entered on the birth certificate shall contain only English alphabetic characters, hyphens, and apostrophes. Other characters including numbers, periods, symbols, or non-English alphabetic characters may not be used.",
+    allowed: /^[A-Za-z'-]+$/,
+  },
+};
